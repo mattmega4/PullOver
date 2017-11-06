189c98e17123fe10f6564bf73c63dcc5e6a3469b
mattmega4 <mattmega4@gmail.com>
2017-11-05 21:14:36 -0800

    setting up everything

---
+++ PullOver/Controller/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  PullOver
+//
+//  Created by Matthew Howes Singleton on 11/5/17.
+//  Copyright © 2017 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
